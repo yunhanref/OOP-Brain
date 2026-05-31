8e7438e94346986f3f378e6ce31d3c9e86de42de
Mehmet Eren Gümüş <yunhanref@proton.me>
2026-05-31 19:39:51 +0300

    Update Activations.h

--- src/Activations.h
+++ src/Activations.h
@@ -2,29 +2,23 @@
 #include "IActivation.h"
 #include <cmath>
 
-// 1. Sigmoid Sınıfı Uygulaması
 class Sigmoid : public IActivation {
 public:
     double transform(double val) const noexcept override {
-        // Formül: 1 / (1 + e^-x)
         return 1.0 / (1.0 + std::exp(-val));
     }
 };
 
-// 2. ReLU Sınıfı Uygulaması
 class ReLU : public IActivation {
 public:
     double transform(double val) const noexcept override {
-        // Formül: max(0, x)
         return (val > 0.0) ? val : 0.0;
     }
 };
 
-// 3. Tanh Sınıfı Uygulaması 
 class Tanh : public IActivation {
 public:
     double transform(double val) const noexcept override {
-        // Formül: tanh(x)
         return std::tanh(val);
     }
 };
