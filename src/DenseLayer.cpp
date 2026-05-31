75726ca78fa871ccffd14925ec98134d227e9d96
Mehmet Eren Gümüş <yunhanref@proton.me>
2026-05-31 19:40:54 +0300

    Update DenseLayer.cpp

--- src/DenseLayer.cpp
+++ src/DenseLayer.cpp
@@ -1,10 +1,23 @@
 #include "DenseLayer.h"
 
 DenseLayer::DenseLayer(int in_dim, int out_dim, IActivation* act) 
-    : weights(in_dim, out_dim), bias(1, out_dim), actFunc(act) {}
+    : weights(out_dim, in_dim), bias(out_dim, 1), actFunc(act) {
+    // Kurucu fonksiyon matrisleri 0 ile baslatir. 
+    // Gercek projede bu agirliklara rastgele deger atamasi yapan bir fonksiyon yazilmalidir.
+}
 
 DenseLayer::~DenseLayer() noexcept {}
 
-Matrix DenseLayer::process(const Matrix& in) {
-    return Matrix(0, 0);
+Matrix DenseLayer::forward(const Matrix& in) {
+    // 1. Lineer Donusum (Z = W * X + b)
+    Matrix z = weights * in + bias;
+    
+    // 2. Aktivasyon Fonksiyonu (A = sigma(Z))
+    Matrix a(z.rows, z.cols);
+    for (int i = 0; i < z.rows; i++) {
+        for (int j = 0; j < z.cols; j++) {
+            a.at(i, j) = actFunc->transform(z.at(i, j));
+        }
+    }
+    return a;
 }
