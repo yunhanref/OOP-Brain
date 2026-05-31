876906c648d03e8e3e784b32562fd749a07a8196
Mehmet Eren Gümüş <yunhanref@proton.me>
2026-05-31 19:38:58 +0300

    Update Matrix.h

--- src/Matrix.h
+++ src/Matrix.h
@@ -1,16 +1,26 @@
 #pragma once
+#include "Exceptions.h"
 
 class Matrix {
 public:
     int rows;
     int cols;
-    double* data; 
+    double* data;
 
     Matrix(int r, int c);
     ~Matrix() noexcept;
+    
+    // Copy ve Move kurallari
     Matrix(const Matrix& other);
+    Matrix(Matrix&& other) noexcept;
     Matrix& operator=(const Matrix& other);
+    Matrix& operator=(Matrix&& other) noexcept;
 
+    // Eleman erisimi
+    double& at(int r, int c);
+    const double& at(int r, int c) const;
+
+    // Operator asiri yuklemeleri
     Matrix operator*(const Matrix& other) const;
-    Matrix operator+(const Matrix& other) const;
+    Matrix operator+(const Matrix& other) const; 
 };
