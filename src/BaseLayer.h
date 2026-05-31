9dd510fd88f2ae850b09358c97ea270295980ad5
Grup7-Proje <grup7proje@proton.me>
2026-05-31 22:42:52 +0300

    Delete src directory

--- src/BaseLayer.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "Matrix.h"
-
-class BaseLayer {
-public:
-    virtual ~BaseLayer() noexcept = default; 
-    virtual Matrix forward(const Matrix& in) = 0; 
-};
--- src/IActivation.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-class IActivation {
-public:
-    virtual ~IActivation() = default;
-    virtual double transform(double val) const noexcept = 0; 
-};
